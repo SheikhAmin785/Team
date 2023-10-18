8eb1927c7546ce9453694378d4ca6d3657829a0d
SheikhAmin785 <shakil785ks@gmail.com>
2023-10-18 22:29:57 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, i,n, c = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for( i = 0; i <n; i++)
+            {
+                string[] st = Console.ReadLine().Split(' ');
+                x = Convert.ToInt32(st[0]);
+                y = Convert.ToInt32(st[1]);
+                z = Convert.ToInt32(st[2]);
+
+                if ((x == 1 && y == 1 && z == 1) ||
+                (x == 0 && y == 1 && z == 1) ||
+                (x == 1 && y == 1 && z == 0) ||
+                (x == 1 && y == 0 && z == 1))
+                    c = c + 1;
+
+            }
+            Console.WriteLine(c);
+            
+        }
+    }
+}
